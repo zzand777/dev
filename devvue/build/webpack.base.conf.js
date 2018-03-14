af476c60dd80f07c83a947c7777f98a29eb2d066
zzand777 <zzand777@naver.com>
2018-03-14 09:58:22 +0900

    IE에서 Promise 사용 할 수 있게 설정

--- devvue/build/webpack.base.conf.js
+++ devvue/build/webpack.base.conf.js
@@ -3,6 +3,7 @@ const path = require('path')
 const utils = require('./utils')
 const config = require('../config')
 const vueLoaderConfig = require('./vue-loader.conf')
+require('es6-promise/auto');
 
 function resolve (dir) {
   return path.join(__dirname, '..', dir)
