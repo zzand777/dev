c7c5f215d997b393e9fbb535c377981165509edd
zzand777 <zzand777@naver.com>
2018-03-17 17:10:17 +0900

    파일 추가

---
+++ devvue/src/components/page/main/DevMain.vue
@@ -0,0 +1,24 @@
+<template>
+  <div>
+      dev-main
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'dev-main',
+  mounted () {
+    this.axios.post('', {})
+    .then(res => {
+      console.log(res)
+    })
+    .catch(err => {
+      console.log(err)
+    })
+  }
+}
+</script>
+
+<style scoped>
+
+</style>
