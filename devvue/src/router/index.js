b93bd8dfda94e5cbfe973fc2f5775c9708a8c785
zzand777 <zzand777@naver.com>
2018-03-17 13:22:13 +0900

    메인 및 로그인 라우터 추가

--- devvue/src/router/index.js
+++ devvue/src/router/index.js
@@ -1,15 +1,28 @@
 import Vue from 'vue'
 import Router from 'vue-router'
 import DevIndex from '@/components/page/index/DevIndex'
+import DevMain from '@/components/page/main/DevMain'
+import KakaoLoginResult from '@/components/kakao/KakaoLoginResult'
 
 Vue.use(Router)
 
 export default new Router({
+  mode: 'history',
   routes: [
     {
       path: '/',
       name: 'index',
       component: DevIndex
+    },
+    {
+      path: '/main',
+      name: 'main',
+      component: DevMain
+    },
+    {
+      path: '/KakaoLoginResult',
+      name: 'KakaoLoginResult',
+      component: KakaoLoginResult
     }
   ]
 })
